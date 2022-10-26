c921f23bacad07d41fa6c258c9f9e3cf9f1302b3
cmeng-git <cmeng.gm@gmail.com>
2022-10-26 11:17:37 +0800

    see RN2.0.0 (20221026)

--- apk_create_apk_remote.sh
+++ apk_create_apk_remote.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# This script is not further required with assetPacks = [":asset_hymnchtv_104000"] consolidated into the main asset
 echo "### Generate single monolithic hymnchtv-debug.apk from ./debug/hymnchtv-debug.aab, for remote installation ###"
 
 java -jar ../bundletool.jar build-apks --bundle=./hymnchtv/build/outputs/bundle/debug/hymnchtv-debug.aab --output=./hymnchtv/build/outputs/apk/debug/hymnchtv-debug2.apks --overwrite --local-testing --mode=universal
